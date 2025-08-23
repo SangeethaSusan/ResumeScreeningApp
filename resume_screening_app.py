3d3e94d160aa37b93478646185d27abcbdb3bd20
Sangeetha Susan John <sangeethasusanjohn@gmail.com>
2025-08-23 15:47:35 +0530

    resume_screening_app.py

--- resume_screening_app.py
+++ resume_screening_app.py
@@ -51,22 +51,23 @@ if st.button("Predict"):
 # Example: your model predicts which categories matched
 
 
-     matched_categories = ['Data Science','HR','Advocate', 'Arts', 'Web Designing', 'Mechanical Engineer', 
-                          'Sales', 'Health and fitness', 'Civil Engineer',
-                          'Java Developer', 'Business Analyst', 'SAP Developer', 'Automation Testing',
-                          'Electrical Engineering', 'Operations Manager', 'Python Developer',
-                          'DevOps Engineer', 'Network Security Engineer', 'PMO', 'Database', 'Hadoop',
-                          'ETL Developer', 'DotNet Developer', 'Blockchain', 'Testing']
-
+         matched_categories = ['Data Science','HR','Advocate', 'Arts', 'Web Designing', 'Mechanical Engineer', 
+                              'Sales', 'Health and fitness', 'Civil Engineer',
+                              'Java Developer', 'Business Analyst', 'SAP Developer', 'Automation Testing',
+                              'Electrical Engineering', 'Operations Manager', 'Python Developer',
+                              'DevOps Engineer', 'Network Security Engineer', 'PMO', 'Database', 'Hadoop',
+                              'ETL Developer', 'DotNet Developer', 'Blockchain', 'Testing']
     
-        # Suppose your model predicts a number
-        predicted_number = model.predict(vector_input)[0]  # e.g., 0, 1, 2 ...
-
-        for predicted_category_name in predicted_number:
-        # Map to name
-            predicted_category_name = matched_categories[predicted_number]
         
-            st.success(f"Matched Category: **{predicted_category_name}**")
+            # Suppose your model predicts a number
+            predicted_number = model.predict(vector_input)[0]  # e.g., 0, 1, 2 ...
+    
+            for predicted_category_name in predicted_number:
+            # Map to name
+                predicted_category_name = matched_categories[predicted_number]
+            
+                st.success(f"Matched Category: **{predicted_category_name}**")
+
 
 
 
