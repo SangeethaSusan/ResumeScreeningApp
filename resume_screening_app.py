baff4f3f6496fc3377fee6bbc330ee6164a532b0
Sangeetha Susan John <sangeethasusanjohn@gmail.com>
2025-08-23 15:37:42 +0530

    resume_screening_app.py

--- resume_screening_app.py
+++ resume_screening_app.py
@@ -39,8 +39,17 @@ if st.button("Predict"):
         category = model.predict(vector_input)[0]
         st.success(f"Predicted Category: **{category}**")
 
-        st.subheader("Matched Categories")
-        st.write(f"- {category}")
+        # Suppose your model predicts a number
+        predicted_number = model.predict(vector_input)[0]  # e.g., 0, 1, 2 ...
+        
+        # Map to name
+        predicted_category_name = matched_categories[predicted_number]
+        
+        st.success(f"Predicted Category: **{predicted_category_name}**")
+
+        st.subheader("Matched Category")
+        st.write(f"- {predicted_category_name}")
+
 
 
 
@@ -52,6 +61,7 @@ if st.button("Predict"):
             st.error(f"Resume Status: **REJECTED** ({skills_matched} skills matched)")
 
 # Example: your model predicts which categories matched
+
 
 
 
