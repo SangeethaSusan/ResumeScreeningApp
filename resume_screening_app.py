f1a02e7c5d4456bb0f764b01f7e7156c4a17e7a1
Sangeetha Susan John <sangeethasusanjohn@gmail.com>
2025-09-03 20:53:40 +0530

    resume_screening_app.py

--- resume_screening_app.py
+++ resume_screening_app.py
@@ -55,9 +55,10 @@ if st.button("Submit"):
         # Check criteria
         skills_matched = check_skills(clean_text)
         if skills_matched >= MIN_SKILLS_MATCH:
-            st.success(f"Resume Status: **ACCEPTED** ({skills_matched} skills matched)")
+            st.success(f"Resume Status: **ACCEPTED** ")
         else:
-            st.error(f"Resume Status: **REJECTED** ({skills_matched} skills matched)")
+            st.error(f"Resume Status: **REJECTED** ")
+
 
 
 
